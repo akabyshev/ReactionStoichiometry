aff0f913a366d11d03e796ae1db988abe13cf0cf
Aziz K <akabyshev@users.noreply.github.com>
2023-11-14 21:45:10 +0600

    del misc

--- ReactionStoichiometry.CLI/BatchProcessor.cs
+++ ReactionStoichiometry.CLI/BatchProcessor.cs
@@ -14,7 +14,10 @@
                 using StreamWriter writer = new(ConstructPath(format.ToString(), type.Name));
 
                 if (format == Balancer.OutputFormat.Json)
+                {
                     writer.WriteLine(value: "{");
+                    writer.WriteLine(value: "\"serialized\": [");
+                }
                 while (reader.ReadLine() is { } line)
                 {
                     if (line.StartsWith(IGNORED_LINE_MARK) || line.Length == 0)
@@ -33,17 +36,21 @@
                             writer.WriteLine(line);
                             break;
                         case Balancer.OutputFormat.DetailedMultiline: break;
-                        case Balancer.OutputFormat.Json:
-                            writer.Write(value: $"\"{type.Name}\": ");
-                            break;
+                        case Balancer.OutputFormat.Json: break;
                         default: throw new ArgumentOutOfRangeException(nameof(format));
                     }
 
-                    writer.WriteLine(balancer.ToString(format));
-                    writer.WriteLine();
+                    writer.Write(balancer.ToString(format));
+                    if (format != Balancer.OutputFormat.Json)
+                        writer.WriteLine(Environment.NewLine);
+                    else
+                        writer.WriteLine(',');
                 }
                 if (format == Balancer.OutputFormat.Json)
+                {
+                    writer.WriteLine(value: ']');
                     writer.WriteLine(value: '}');
+                }
             }
         }
 
