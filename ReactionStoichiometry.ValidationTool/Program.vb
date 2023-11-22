25eebe78a81ceefd80249d83d979512eb61e3bfc
Aziz K <akabyshev@users.noreply.github.com>
2023-11-22 13:58:35 +0600

    external validator is ready. gonna try Lazy<> solutions

--- ReactionStoichiometry.ValidationTool/Program.vb
+++ ReactionStoichiometry.ValidationTool/Program.vb
@@ -1,3 +1,7 @@
+Imports System.Numerics
+Imports System.Text.RegularExpressions
+Imports Rationals
+
 Module Program
     Sub Main(args As String())
         Dim equationString As String
@@ -11,15 +15,41 @@ Module Program
 
         If equationString = "" Then
             equationString =
-                $"10·K4Fe(CN)6 + 122·KMnO4 + 299·H2SO4 = 162·KHSO4 + 5·Fe2(SO4)3 + 122·MnSO4 + 60·HNO3 + 60·CO2 + 188·H2O"
+                $"2·C6H5COOH + 15·O2 = 14·CO2 + 6·H2O"
         End If
 
         If Not IsReactionEquationBalanced(equationString) Then
-            Environment.ExitCode = 1
+            Environment.ExitCode = -1
         End If
+
     End Sub
 
-    Private Function IsReactionEquationBalanced(eq)
-        IsReactionEquationBalanced = Len(eq) > 2
+    Private Function IsReactionEquationBalanced(eq As String)
+        Dim parts = eq.Split("="c, "+"c)
+        Dim border = eq.Split("="c)(0).Split("+"c).Length
+        Dim coefficients As List(Of BigInteger) = New List(Of BigInteger)()
+        For i As Integer = LBound(parts) To UBound(parts) Step 1
+            Dim part As String = parts(i)
+            Dim coefstr As String = IIf(part.Contains(GlobalConstants.MULTIPLICATION_SYMBOL),
+                              part.Split(GlobalConstants.MULTIPLICATION_SYMBOL)(0), "1")
+            Dim coefrat As Rational = Rational.ParseDecimal(coefstr) * IIf(i < border, New Rational(-1), New Rational(1))
+            If (coefrat.Denominator <> 1) Then
+                Throw New FormatException
+            End If
+            coefficients.Add(coefrat.Numerator)
+        Next
+
+        Dim skeletal As String = Regex.Replace(eq, "\d+·", "")
+        Console.WriteLine("Skeletal:" + skeletal)
+        Console.WriteLine("Solution:" + eq)
+
+        Dim equation As New ChemicalReactionEquation(skeletal)
+        Dim verdict = equation.Validate(coefficients.ToArray())
+        Console.WriteLine()
+        Console.WriteLine(IIf(verdict, "Solution OK", "IT IS NOT BALANCED"))
+        Console.WriteLine()
+        Console.WriteLine()
+
+        IsReactionEquationBalanced = verdict
     End Function
 End Module
